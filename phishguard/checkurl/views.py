4dfe149b1d8e896c3450270e81aa87acaaee0625
Josefina Baro <j_barofranco@mail.fhsu.edu>
2024-12-11 23:48:59 -0600

    updated register.html, users register and it redirects to userdash

--- phishguard/checkurl/views.py
+++ phishguard/checkurl/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .api import check_url_with_virustotal
-from django.conf import settings
-import requests
+from django.contrib.auth.decorators import login_required
 
 # Home page
 def home(request):
@@ -23,17 +20,40 @@ def awareness(request):
 # Registration page
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Your account has been created!')
-            return redirect('userdash')
-        else:
-            messages.error(request, 'Could not register user.')
+        if 'register' in request.POST:
+            return handle_registration(request)
+        elif 'login' in request.POST:
+            return handle_login(request)
+
     else:
         form = UserCreationForm()
+
     return render(request, 'phishguard/register.html', {'form': form})
+
+
+def handle_registration(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        messages.success(request, 'Your account has been created successfully!')
+        return redirect('userdash')
+    else:
+        messages.error(request, 'Could not register user. Please try again.')
+        return render(request, 'phishguard/register.html', {'form': form})
+
+
+def handle_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('userdash')
+    else:
+        messages.error(request, 'Invalid credentials. Please try again.')
+        return render(request, 'phishguard/register.html')
+
 @login_required
 def userdash(request):
     return render(request, 'phishguard/userdash.html')
